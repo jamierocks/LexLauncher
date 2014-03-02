6a459fca16148b3b5c5898174ea4930f8cdf6e89
Jamie <jamieflargm@icloud.com>
2014-03-02 12:28:35 +0000

    Added a config file

--- src/malgm/minecraft/versioninstaller/util/MinecraftForgeVersionGetter.java
+++ src/malgm/minecraft/versioninstaller/util/MinecraftForgeVersionGetter.java
@@ -1,9 +1,32 @@
 package malgm.minecraft.versioninstaller.util;
 
+import java.io.*;
+import java.util.Properties;
+
 public class MinecraftForgeVersionGetter {
 	
+	private String part1 = null;
+	private String part2 = null;
+	
 	public String getURL(String version) {
-		return "http://files.minecraftforge.net/maven/net/minecraftforge/forge/" + version + "/" + version + "-universal.jar";
+		
+		Properties prop = new Properties();
+		InputStream input = null;
+		
+		try {
+			input = new FileInputStream("config.properties");
+			
+			prop.load(input);
+			
+			part1 = prop.getProperty("mcfpart1");
+			part2 = prop.getProperty("mcfpart2");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		System.out.println(part1 + version + "/" + version + part2);
+		
+		return part1 + version + "/" + version + part2;
 	}
 
 }
