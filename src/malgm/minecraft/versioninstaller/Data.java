86426b77a57a06753485a2a4d2a1da94ce74b11b
Jamie <jamieflargm@icloud.com>
2014-04-10 21:03:56 +0100

    Removed Help UI under mods list

--- src/malgm/minecraft/versioninstaller/Data.java
+++ src/malgm/minecraft/versioninstaller/Data.java
@@ -2,7 +2,7 @@ package malgm.minecraft.versioninstaller;
 
 public class Data {
 	
-	private double version = 1.012;
+	private double version = 1.013;
 
 	public double getVersion() {
 		return version;
--- src/malgm/minecraft/versioninstaller/ui/modslist/ModsListUI.java
+++ src/malgm/minecraft/versioninstaller/ui/modslist/ModsListUI.java
@@ -4,10 +4,7 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-import javax.swing.JFrame;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
+import javax.swing.*;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -22,11 +19,6 @@ public class ModsListUI extends JFrame implements ActionListener {
 	
 	private String[] modslist = null, urllist = null;
 	
-	private JMenuBar menuBar;
-	
-	private JMenu file;
-	private JMenuItem help;
-	
 	public ModsListUI() {
 		init();
 	}
@@ -34,21 +26,8 @@ public class ModsListUI extends JFrame implements ActionListener {
 	public void init() {
 		setLayout(new FlowLayout());
 		
-		// init menu bar
-		menuBar = new JMenuBar();
-		
-		// init file on menu bar
-		file = new JMenu("File");
-		menuBar.add(file);
-		
-		// init help under file on menu bar
-		help = new JMenuItem("Help");
-		help.addActionListener(this);
-		file.add(help);
-		
 		ModsListContainer c = new ModsListContainer();
 		
-		setJMenuBar(menuBar);
 		add(c);
 	}
 
@@ -96,14 +75,6 @@ public class ModsListUI extends JFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent event) {
-		if(event.getSource() == this.help) {
-			HelpUI helpui = new HelpUI();
-			
-			helpui.setTitle("Help");
-			helpui.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
-			helpui.setSize(200, 300);
-			helpui.setResizable(false);
-			helpui.setVisible(true);
-		}
+		
 	}
 }
