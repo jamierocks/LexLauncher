203337cdee21c1abdbf4d85350efb54d264987e3
Jamie <lexwaresupport@outlook.com>
2014-05-03 22:31:44 +0100

    More work on modpacks tab

--- src/malgm/minecraft/launcher/ui/tabs/modpacks/Modpack.java
+++ src/malgm/minecraft/launcher/ui/tabs/modpacks/Modpack.java
@@ -1,13 +1,41 @@
 package malgm.minecraft.launcher.ui.tabs.modpacks;
 
+import java.awt.*;
+import java.awt.event.ActionListener;
+
 import javax.swing.*;
+
+import malgm.minecraft.launcher.ResourceLoader;
+import malgm.minecraft.launcher.ui.TechUI;
+import malgm.minecraft.launcher.util.Utils;
 
 public class Modpack extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
 	
-	public Modpack(String name, String actionCommand, String imageDir) {
+	public Modpack(String name, String actionCommand, String imageName, ActionListener listener, ResourceLoader resLoader) {
+		setOpaque(false);
 		
+		Image modpackicon = Toolkit.getDefaultToolkit().getImage(Utils.getLauncherDirectory() + "/resources/modpacks/" + imageName + ".png");
+		JButton icon = new JButton(new ImageIcon(modpackicon));
+		icon.setBorder(BorderFactory.createEmptyBorder());
+		icon.setFocusable(false);
+		icon.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		icon.addActionListener(listener);
+		icon.setActionCommand(actionCommand);
+		
+		JButton nameButton = new JButton(name);
+		nameButton.setForeground(TechUI.COLOR_WHITE_TEXT);
+		nameButton.setFont(resLoader.getFont(ResourceLoader.FONT_RALEWAY, 26));
+		nameButton.setBorder(BorderFactory.createEmptyBorder());
+		nameButton.setContentAreaFilled(false);
+		nameButton.setFocusable(false);
+		nameButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+		nameButton.addActionListener(listener);
+		nameButton.setActionCommand(actionCommand);
+		
+		add(icon);
+		add(nameButton);
 	}
 
 }
--- src/malgm/minecraft/launcher/ui/tabs/modpacks/ModpacksSelector.java
+++ src/malgm/minecraft/launcher/ui/tabs/modpacks/ModpacksSelector.java
@@ -1,20 +1,43 @@
 package malgm.minecraft.launcher.ui.tabs.modpacks;
 
-import java.awt.BorderLayout;
-import java.awt.Color;
+import java.awt.*;
 
 import javax.swing.*;
 
 import malgm.minecraft.launcher.ResourceLoader;
+import malgm.minecraft.launcher.ui.components.SimpleScrollBarUI;
 
 public class ModpacksSelector extends JPanel {
 	
 	private static final long serialVersionUID = 1L;
+	private JPanel widgetList;
 	
 	public ModpacksSelector(ResourceLoader resLoader) {
 		setLayout(new BorderLayout());
-		
 		setBackground(new Color(32, 32, 32));
+		
+		// list of modpacks
+		widgetList = new JPanel();
+        widgetList.setOpaque(false);
+        widgetList.setLayout(new FlowLayout());
+		
+        // scroll bar of modpacks
+		JScrollPane scrollPane = new JScrollPane(widgetList, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+		scrollPane.setOpaque(false);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.getViewport().setOpaque(false);
+		scrollPane.getVerticalScrollBar().setUI(new SimpleScrollBarUI());
+		scrollPane.getVerticalScrollBar().setPreferredSize(new Dimension(10, 10));
+        scrollPane.getVerticalScrollBar().setUnitIncrement(12);
+		
+        // test modpack
+		Modpack malgmpack = new Modpack("malgm_pack", "malgmPack", "malgmPack", null, resLoader);
+		//Modpack addNewPack = new Modpack("Add new pack", "addNewPack", "addNewPack", null, resLoader);
+		widgetList.add(malgmpack);
+		//widgetList.add(addNewPack);
+		
+		// add scroll bar to panel
+		add(scrollPane, BorderLayout.CENTER);
 	}
 
 }
